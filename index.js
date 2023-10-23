6e70fae03555b0ff90d1804d5b545ff1fa1dc00c
mike-jshen <mike.shen@outlook.com>
2023-10-23 16:07:51 -0400

    remove three.js
    i want to separate three.js from this project for now as i want to focus on this website alone without cluttering two things at once

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-// import * as THREE from "three";
-// import stylesheet from './format.css';
 
 // you have to import filepath
 const __filename = fileURLToPath(import.meta.url);
@@ -30,50 +28,3 @@ app.get('/upload.html', (req, res) => {
 
 app.listen(port);
 console.log(`Server started at http://localhost:${port}`);
-
-
-//Constructing the overall scene and choosing the color
-// const scene = new THREE.Scene();
-// scene.background = new THREE.Color(0x000000);
-
-// Add a cube to the scene
-// const geometry = new THREE.BoxGeometry(3, 1, 3); // width, height, depth
-// const material = new THREE.MeshLambertMaterial({ color: 0xfb8e00 });
-// const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.set(0, 0, 0); // Optional, 0,0,0 is the default
-// scene.add(mesh);
-
-
-// Set up lights
-// const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-// scene.add(ambientLight);
-
-// const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
-// dirLight.position.set(10, 20, 0); // x, y, z
-// scene.add(dirLight);
-
-// Camera
-// const width = 10;
-// const height = width * (window.innerHeight / window.innerWidth);
-// const camera = new THREE.OrthographicCamera(
-//   width / -2, // left
-//   width / 2, // right
-//   height / 2, // top
-//   height / -2, // bottom
-//   1, // near
-//   100 // far
-// );
-
-// NO CAMERA IF WINDOW IS NOT DEFINED
-// camera.position.set(4, 4, 4);
-// camera.lookAt(0, 0, 0);
-
-// RENDERER is NOT defined 
-
-// Renderer
-// const renderer = new THREE.WebGLRenderer({ antialias: true });
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// renderer.render(scene, camera);
-
-// Add it to HTML
-// document.body.appendChild(renderer.domElement);
