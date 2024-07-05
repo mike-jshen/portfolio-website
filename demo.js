7f94af46d027513ca4471c44c75aa5a32fcd4231
mike-jshen <mike.shen@outlook.com>
2024-07-05 10:03:51 -0400

    testing out old index.html file under "public" folder

--- demo.js
+++ demo.js
@@ -24,6 +24,13 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, './about.html'));
 });
 
+app.get('/public/index', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// the same logic also must be used to import the images into the nodejs file
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 // app.get('/items', (req, res) => {
 //     res.json({ status: true, message: "Fetched all items", data: items })
 // })
