9eae44bfef9890fe10c4a2c9b8fc6f13ea7388bf
mike-jshen <mike.shen@outlook.com>
2024-08-08 16:10:56 -0400

    node de

--- demo.js
+++ demo.js
@@ -28,8 +28,20 @@ app.get('/public/index', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/first_3d/line', (req, res) => {
+    res.sendFile(path.join(__dirname, './first_3d/line.html'));
+});
+
+app.get('/first_3d/sphere', (req, res) => {
+    res.sendFile(path.join(__dirname, './first_3d/sphere.html'));
+});
+
 // the same logic also must be used to import the images into the nodejs file
 app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+// using express, i can access js files in specified directories
+app.use(express.static(__dirname + '/'));
 
 // app.get('/items', (req, res) => {
 //     res.json({ status: true, message: "Fetched all items", data: items })
